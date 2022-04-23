29e47a29ec69a686e88bb73e506c7280a58277f6
Noah Hdez <noah.hernandez101@alu.ulpgc.es>
2022-04-23 21:06:17 +0100

    Added UI for O and Fuel

--- Assets/Scripts/SpaceShip/SpaceShip.cs
+++ Assets/Scripts/SpaceShip/SpaceShip.cs
@@ -1,12 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpaceShip : MonoBehaviour
 {
     protected List<SystemBase> systems;
     protected float fuel = 100;
     public const int maxFuel = 100;
+    public Slider fuelSlider;
+
+    private void Start()
+    {
+        fuelSlider.maxValue = maxFuel;
+        fuelSlider.value = fuel;
+    }
 
     public float GetFuel()
     {
@@ -16,11 +24,13 @@ public class SpaceShip : MonoBehaviour
     public void AddFuel(float fuel)
     {
         this.fuel = ((this.fuel+fuel) > maxFuel) ? maxFuel : this.fuel+fuel;
+        fuelSlider.value = fuel;
     }
 
     public void RemoveFuel(float fuel)
     {
         this.fuel = ((this.fuel - fuel) < 0) ? 0 : this.fuel - fuel;
+        fuelSlider.value = fuel;
     }
 
     public void AddSystem(SystemBase system)
--- Assets/Scripts/Systems/SistemaOxigeno.cs
+++ Assets/Scripts/Systems/SistemaOxigeno.cs
@@ -1,16 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SistemaOxigeno : SystemBase
 {
     protected float oxigenLevel = 100;
     protected float maxOxigenLevel = 100;
+    public Slider oxygenSlider;
+
+    private void Start()
+    {
+        oxygenSlider.maxValue = maxOxigenLevel;
+        oxygenSlider.value = oxigenLevel;
+    }
 
     public override void TakeDamage(int damage)
     {
-        this.life = ((this.life - damage) < 0) ? 0 : this.life - damage;
-        this.oxigenLevel = ((this.oxigenLevel - damage * 10) < 0) ? 0 : this.oxigenLevel - damage * 10;
+        life = ((life - damage) < 0) ? 0 : life - damage;
+        oxigenLevel = ((oxigenLevel - damage * 10) < 0) ? 0 : oxigenLevel - damage * 10;
         if(oxigenLevel <= 0)
         {
             GameObject [] chanos = GameObject.FindGameObjectsWithTag("Chano");
